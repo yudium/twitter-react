8226db232c614249e712594852e9821c27c32ac5
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2020-01-04 09:03:11 +0700

    CSS Change

--- src/components/navigations/SidebarNavigation.js
+++ src/components/navigations/SidebarNavigation.js
@@ -4,12 +4,12 @@ import { Nav, Button } from 'react-bootstrap';
 import logo from '../../logo.png';
 
 const SidebarNavigation = (props) => (
-	<React.Fragment>
-		<div id="logo-container">
-			<img id="logo" src={logo} alt="twitter logo" width="28" height="28" />
+	<div id="SidebarNavigation">
+		<div class="logo-container">
+			<img src={logo} alt="twitter logo" width="28" height="28" />
 		</div>
 		
-		<Nav defaultActiveKey={props.location.pathname} className="flex-column">
+		<Nav defaultActiveKey={props.location.pathname} id="main-menu" className="flex-column">
 			<Nav.Link href="/home">Home</Nav.Link>
 			<Nav.Link href="/home">Explore</Nav.Link>
 			<Nav.Link href="/home">Notifications</Nav.Link>
@@ -18,8 +18,8 @@ const SidebarNavigation = (props) => (
 			<Nav.Link href="/home">Profile</Nav.Link>
 		</Nav>
 
-		<Button type="button">Tweet</Button>
-	</React.Fragment>
+		<Button type="button" block>Tweet</Button>
+	</div>
 );
 
 SidebarNavigation.propTypes = {
